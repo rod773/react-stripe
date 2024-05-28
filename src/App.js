bc2c7955b4487b19385afe6fe1a97f5173ff076c
rod7773 <rodrigoarcadio1@gmail.com>
2024-05-28 17:18:43 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react';
-import './styles/App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from "react";
+import "./styles/App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { useDispatch } from 'react-redux';
-import Home from './components/pages/Home/Home';
-import Menu from './components/molecules/Menu/Menu';
-import Cart from './components/pages/Cart/Cart';
-import Checkout from './components/pages/Checkout/Checkout';
-import { getCartFromStorage } from './store/actions/cartActions';
-import Success from './components/pages/Success/Success';
+import { useDispatch } from "react-redux";
+import Home from "./components/pages/Home/Home";
+import Menu from "./components/molecules/Menu/Menu";
+import Cart from "./components/pages/Cart/Cart";
+import Checkout from "./components/pages/Checkout/Checkout";
+import { getCartFromStorage } from "./store/actions/cartActions";
+import Success from "./components/pages/Success/Success";
 
 function App() {
   const dispatch = useDispatch();
@@ -21,7 +21,6 @@ function App() {
   return (
     <Router>
       <Menu />
-
       <Routes>
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="/checkout" element={<Checkout />}></Route>
